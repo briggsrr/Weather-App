d0c8b1a04f053c08ce488171d5946d3eb22348f4
briggsrr <robert.briggs.3@icloud.com>
2022-07-25 17:25:02 -0700

    Integrated search bar

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7fa081e53bmsh4054f7caaac3182p1dffe8jsna17d52de46e1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/";
+
