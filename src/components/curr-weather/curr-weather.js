751ff19fe1295a0fa4d2e58c5d8315ed4bdc742c
briggsrr <robert.briggs.3@icloud.com>
2022-07-27 16:07:14 -0700

    Started first widget

--- src/components/curr-weather/curr-weather.js
+++ src/components/curr-weather/curr-weather.js
@@ -1,13 +1,17 @@
+import React from "react";
 import "./curr-weather.css"
 
 const CurrWeather = () => {
     return (
         <div className="weather">
             <div className="top">
-                <p className="city">Belgrade </p>
-                <p className="weather-desc">Sunny </p>
-            </div>
+                <div>
+                    <p className="city">Belgrade </p>
+                    <p className="weather-desc">Sunny </p>
+                </div>
+            
             <img alt ="weather" className="weather-icon" src ="icons/01d.png"/>
+            </div>
         </div>
     );
 
