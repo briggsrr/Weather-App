ce642b383a2ecc8ab52227e40c8dc2d3aa9ecd7c
briggsrr <robert.briggs.3@icloud.com>
2022-07-27 18:05:29 -0700

    Laying foundation for API weather calls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import './App.css';
 import CurrWeather from './components/curr-weather/curr-weather';
 import Footer from './components/Footer';
@@ -9,8 +10,21 @@ import Titlebar from './components/Titlebar/Titlebar';
 
 function App() {
 
+  const [currentWeatherFetch, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] =useState(null);
+  
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+  
+    const currentWeatherFetch = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3480952f6cefaf7a9cee71c7360d3a0d`)
+    const forecastFetch = fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=3480952f6cefaf7a9cee71c7360d3a0d`);
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) =>  {
+        const weatherResponse = await response[0].json(); 
+        const forecastResponse = await response[0].json();     
+      })
+
   }
 
   return (
--- src/components/curr-weather/curr-weather.js
+++ src/components/curr-weather/curr-weather.js
@@ -8,12 +8,37 @@ const CurrWeather = () => {
                 <div>
                     <p className="city">Belgrade </p>
                     <p className="weather-desc">Sunny </p>
+                    
                 </div>
             
             <img alt ="weather" className="weather-icon" src ="icons/01d.png"/>
+            </div>
+            <div className="bottom">
+            <p class ="temp">18°C</p>
+                <div className="details">
+                    <div className="parameter-row">
+                        <span className="parameter-label">Details</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels like</span>
+                        <span className="parameter-value">22°C</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-value">2 m/s</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-value">15%</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure</span>
+                        <span className="parameter-value">15 hPa</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
 
 }
-export default CurrWeather
+export default CurrWeather;
